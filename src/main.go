a641601c03b241a9193d026c65d8db5ac1743abc
Nikhil Parakh <nikhilparakh@Nikhils-MacBook-Pro-2.local>
2023-07-02 16:08:37 -0400

    APIs implemented

---
+++ src/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Item struct {
+	Id               string `json:"id"`
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
+
+type Receipt struct {
+	Id           string `json:"id"`
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
+}
+
+var receipts []Receipt
+
+var receiptPoints = make(map[string]int)
+
+func processReceiptHandler(c *gin.Context) {
+
+	var newReceipt Receipt
+	if err := c.BindJSON(&newReceipt); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	newReceipt.Id = uuid.New().String()
+	receipts = append(receipts, newReceipt)
+	receiptPoints[newReceipt.Id] = processReceiptPoints(newReceipt)
+	c.IndentedJSON(http.StatusCreated, gin.H{"id": newReceipt.Id})
+
+}
+
+func getReceiptPointsByIdHandler(c *gin.Context) {
+	id := c.Param("id")
+	points, exists := receiptPoints[id]
+	if exists {
+		fmt.Printf("points: %v\n", points)
+		c.IndentedJSON(http.StatusFound, gin.H{"points": points})
+	} else {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "No receipt found with id " + id})
+	}
+
+}
+
+func getAllReceiptsHandler(c *gin.Context) {
+	c.IndentedJSON(http.StatusFound, gin.H{"receipts": receipts})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/receipts/process", processReceiptHandler)
+	router.GET("/receipts/:id/points", getReceiptPointsByIdHandler)
+	router.GET("/receipts", getAllReceiptsHandler)
+	router.Run("localhost:8080")
+}
+
+func processReceiptPoints(receipt Receipt) int {
+	var points int
+	points += getNumberOfAlphanumericCharInString(receipt.Retailer)
+	_, cents, err := getDollarAndCentFromPrice(receipt.Total)
+	if err == nil {
+		if cents == 0 {
+			points += 50
+		}
+		if cents%25 == 0 {
+			points += 25
+		}
+	}
+	points += (len(receipt.Items) / 2) * 5
+	for _, item := range receipt.Items {
+		point, err := getPointsFromItemDescription(item)
+		if err == nil {
+			points += point
+		}
+	}
+	pointsFromDateTime, err := getPointsFromDateTime(receipt.PurchaseDate, receipt.PurchaseTime)
+	if err == nil {
+		points += pointsFromDateTime
+	}
+	return points
+}
+
+func getNumberOfAlphanumericCharInString(str string) int {
+	totalAlphaNumeric := 0
+	var alphanumeric = regexp.MustCompile("^[a-zA-Z0-9_]*$")
+	for i := 0; i < len(str); i++ {
+		if alphanumeric.Match([]byte(str[i : i+1])) {
+			totalAlphaNumeric++
+		}
+	}
+	return totalAlphaNumeric
+}
+
+func getDollarAndCentFromPrice(price string) (int, int, error) {
+	priceSplit := strings.Split(price, ".")
+	dollar, err := strconv.Atoi(priceSplit[0])
+	if err != nil {
+		//TODO: check for this status
+		return -1, -1, err
+	}
+	cents, err := strconv.Atoi(priceSplit[1])
+	if err != nil {
+		//TODO: check for this status
+		return -1, -1, err
+	}
+	return dollar, cents, nil
+}
+
+func getPointsFromItemDescription(item Item) (int, error) {
+	itemDesc := strings.TrimSpace(item.ShortDescription)
+	if len(itemDesc)%3 == 0 {
+		priceFloat, err := strconv.ParseFloat(item.Price, 64)
+		if err != nil {
+			return 0, err
+		}
+		roundedPrice := int(math.Round(priceFloat * 0.2))
+		if float64(roundedPrice) < priceFloat {
+			roundedPrice++
+		}
+		return roundedPrice, nil
+	}
+	return 0, nil
+}
+
+func getPointsFromDateTime(date string, timeStr string) (int, error) {
+	dateTimeStr := date + " " + timeStr
+	purchaseDateTime, err := time.Parse("2006-01-02 15:04", dateTimeStr)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return 0, err
+	}
+	var points int
+	if purchaseDateTime.Day()%2 != 0 {
+		fmt.Println("\nODD DAY")
+		points += 6
+	}
+
+	fmt.Printf("HOUR: %v\n", purchaseDateTime.Hour())
+	if purchaseDateTime.Hour() >= 14 && purchaseDateTime.Hour() < 16 {
+		fmt.Println("betw 2 & 4 pm")
+		points += 10
+	}
+	return points, nil
+}
